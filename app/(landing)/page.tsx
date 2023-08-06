7eba3c2e7fdad112522329e1647d48c792ba92da
DarKS4ys <melihyardim1057@gmail.com>
2023-08-06 10:09:29 +0300

    update landing

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,20 +1,34 @@
 import { Button } from '@/components/ui/button';
+import Image from 'next/image';
 import Link from 'next/link';
+import StoicIcon from '@/public/stoicWhite.png';
 
 const LandingPage = () => {
   return (
     <div className='w-full h-full bg-gray-900'>
-      <div className='text-white'>
+
+      <div className='text-white gap-3 flex justify-center h-screen'>
+
+        <div className='flex flex-col items-center justify-center gap-4'>
+        <Image
+        src={StoicIcon}
+        width={120}
+        height={150}
+        alt="Stoic Pic"
+        className='h-15 w-12'
+        />
+        <h1 className='text-4xl font-semibold'>S T O I C</h1>
         Landing Page (Unprotected)
         <Link href="/dashboard">
-          <Button>Dashboard</Button>
+          <Button variant="dark">Dashboard</Button>
         </Link>
         <Link href="/sign-up">
           <Button variant="dark">Sign Up</Button>
         </Link>
         <Link href="/sign-in">
-          <Button>Sign In</Button>
+          <Button variant="dark">Sign In</Button>
         </Link>
+        </div>
       </div>
     </div>
   );
